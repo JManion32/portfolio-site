99f35f14aeef35335f34aa68ad698622a70f7ebf
Justin Manion <jmanion32@gmail.com>
2025-08-12 17:26:29 -0400

    Added Subheaders

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -9,6 +9,12 @@ function Experience() {
       <div className="page">
         <div className="content">
           <h1 className="page-header">Experience</h1>
+          <h2>Rensselaer Polytechnic Institute</h2>
+          <h2>IT Field Technician Intern</h2>
+          <h2>Lambda Chi Alpha</h2>
+          <h2>Price Chopper</h2>
+          <h2>Hudson Valley Community College</h2>
+          <h2>Twitch Streamer</h2>
         </div>
         <footer className="guide-footer">
           <button className="left-guide-button" onClick={() => navigate('/projects')}>

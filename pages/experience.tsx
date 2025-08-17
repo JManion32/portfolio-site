d783999b6e33d708781e83ad204991dbd214b850
Justin Manion <jmanion32@gmail.com>
2025-08-17 19:18:57 -0400

    Fixed Links and Images on Experience Page

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -36,41 +36,41 @@ function Experience() {
                   <p className="experience-date">May 2024 - August 2024</p>
                   <p className="project-preview-desc">On-site technician responsible for troubleshooting and maintaining hardware.</p>
               </div>
-              <div onClick={() => navigate('/experience/Rensselaer')} className="project-preview-container">
+              <div onClick={() => navigate('/experience/LambdaChi')} className="project-preview-container">
                   <img
                       src="../assets/lxa-logo.webp"
                       className="project-image"
-                      alt="Rensselar Polytechnic Institute Seal"
+                      alt="LXA Cross and Crescent Symbol"
                   />
                   <h2 className="project-preview-header">Lambda Chi Alpha</h2>
                   <p className="experience-date">August 2024 - Present</p>
                   <p className="project-preview-desc">Secretary, Webmaster, and Brother.</p>
               </div>
-              <div onClick={() => navigate('/experience/Rensselaer')} className="project-preview-container">
+              <div onClick={() => navigate('/experience/PriceChopper')} className="project-preview-container">
                   <img
                       src="../assets/price-chopper.png"
                       className="project-image"
-                      alt="Rensselar Polytechnic Institute Seal"
+                      alt="Price Chopper Logo"
                   />
                   <h2 className="project-preview-header">Price Chopper</h2>
                   <p className="experience-date">June 2020 - October 2024</p>
                   <p className="project-preview-desc">Shift Supervisor, Office Teammate, Cashier, and Maintenance.</p>
               </div>
-              <div onClick={() => navigate('/experience/Rensselaer')} className="project-preview-container">
+              <div onClick={() => navigate('/experience/HudsonValley')} className="project-preview-container">
                   <img
                       src="../assets/hvcc-green.jpg"
                       className="project-image"
-                      alt="Rensselar Polytechnic Institute Seal"
+                      alt="Hudson Valley Community College Logo"
                   />
                   <h2 className="project-preview-header">Hudson Valley Community College</h2>
                   <p className="experience-date">August 2022 - May 2024</p>
                   <p className="project-preview-desc">Associates Degree in Computer Information Systems.</p>
               </div>
-              <div onClick={() => navigate('/experience/Rensselaer')} className="project-preview-container">
+              <div onClick={() => navigate('/experience/Streamer')} className="project-preview-container">
                   <img
                       src="../assets/twitch-logo.png"
                       className="project-image"
-                      alt="Rensselar Polytechnic Institute Seal"
+                      alt="Twitch Logo"
                   />
                   <h2 className="project-preview-header">Twitch Streamer</h2>
                   <p className="experience-date">December 2020 - November 2023</p>

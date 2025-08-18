ff23abaf9b89c50f8def625589d30d9979071646
Justin Manion <jmanion32@gmail.com>
2025-08-18 14:12:14 -0400

    Added to Speed Roulette Page
    65% completed. Much work to still be done, but I am going to circle back.
    
    Added a bunch of images, refactored content to have background-color for better readability and overall site look.

--- pages/projects/speedroulette.tsx
+++ pages/projects/speedroulette.tsx
@@ -9,7 +9,7 @@ function SpeedRoulette() {
   return (
     <>
       <div className="page">
-        <div className="content">
+        <div className="text-content">
           <Breadcrumbs />
           <div className="header-row">
             <h1 className="page-header">Speed Roulette</h1>
@@ -29,7 +29,7 @@ function SpeedRoulette() {
               </div>
             </button>
           </div>
-          <p>
+          <p className="project-p">
             Inspired by the intensity of speed chess, Speed Roulette puts an interesting twist on the beloved 
             high-stakes casino game. Players start with 20 dollars, 10 possible spins, and just 60 seconds on the 
             clock. When bets are submitted, the winning number is revealed, earnings are paid out, and the clock 
@@ -84,21 +84,80 @@ function SpeedRoulette() {
               }
             ]}
           />
+          <h2 className="page-subheader">Planning</h2>
+          <p className="project-p">The idea of building an online roulette game dates back to 2023 when the only programming language I knew was Java:</p>
+          <div className="project-img-container">
+            <img src="../../assets/javafx-roulette.png" className="project-img"/>
+          </div>
+          <p className="project-p">
+            This was just a GUI I built using my knowledge from Programming and Data Logic 2 at Hudson Valley. It was limited 
+            in functionality, and had a very bloated codebase (I didn't yet know how to utilize OOP). I knew I wanted to create 
+            something more. Something I was proud of. Something I could enjoy with my friends.
+          </p>
+            
+          <p className="project-p">
+            For awhile, I considered just refining what I had already built. After taking Data Structures at RPI, I refactored the
+            codebase and brought the line count down from ~3,000 to ~1,000. At this point, I still didn't know enough about development 
+            to understand that I could not make a website using JavaFX. It wasn't until I joined Submitty in January 2025 that I 
+            was exposed to web technologies.
+          </p>
+
+          <p className="project-p">
+            The lightbulb went off on February 21st, 2025. I had been getting a lot of speed chess content in my YouTube feed and I thought, this 
+            would be really cool to combine with roulette! Being halfway through my semester and having a limited skillset, I decided to take 
+            my time in the planning phase. I created a full storyboard of all pages of the site, figured out my tech stack, and refined the 
+            rules of the game. While planning is important and certainly paid off in the long run, I avoided analysis paralysis and dove 
+            into the implementation.
+          </p>
+
           <h2 className="page-subheader">Implementation</h2>
+          <p className="project-p">With the complete storyboard, it was easy to build out the UI.</p>
+          <div className="project-img-container">
+            <img src="../../assets/sr-storyboard-dark-mode.png" className="project-img"/>
+            <img src="../../assets/sr-home-dark-mode.png" className="project-img"/>
+          </div>
+          <p className="project-p">
+            I then moved to the backend.
+          </p>
 
           <h2 className="page-subheader">Technical Challenges</h2>
-          1. New Technologies - throwing myself in the deep end
-          2. Grid layout
-          3. Deployment
-          4. Security
+
+          <h3>1. New Technologies</h3>
+          <p className="project-p">
+            When I started this project, I don't think I knew a single tool in the finished tech stack. Not one. 
+            It was quite overwhelming at first to day the least. I had to learn HTML, then immediately learn React. 
+            Most front end technologies are easy to figure out, but the backend was a different story.
+          </p>
+          <h3>2. Grid layout</h3>
+          <p>
+            One issue in my original JavaFX GUI was that the user could not place a bet in between table cells (e.g., 7 and 10).
+            To address that here, I created a table of the entire roulette board, and then overlayed CSS grids on top of it. This 
+            took many hours to perfect, but the final product was well worth it.
+
+          </p>
+          <h3>3. Deployment</h3>
+          <p className="project-p">
+            Out of all the blockers I had to work through on this project, deployment was by far the most time consuming. 
+            Of course, this was my first time deploying anything so there was an inevitable learning curve, but that wasn't 
+            the primary issue. I was using AWS, which, for a new developer's personal project, is not a good fit.
+          </p>
+          <h3>4. Security</h3>
+          <p className="project-p">
+            Although I am not handling any sensitive user data, I took great care in site security.
+          </p>
 
           <h2 className="page-subheader">Reflection</h2>
-          Despite this project being created this year, I think this is what truly ignited my true passion
-          for programming. Before this I had created projects for assignments and contributed Submitty, but 
-          I did it for requirement, rather than the love of the game. From the moment I started this project 
-          to the day I deployed it, I was invested, and enjoyed every aspect of the process. I have already 
-          applied so much of what I learned in this project to other projects. I am excited to refine it in 
-          the future as I continue to learn and grow as a developer.
+          <p className="project-p">
+            Although this project was created only this year, I believe it is what truly ignited my passion for programming. 
+            Before this, I had built projects for assignments and contributed to Submitty, but I did so out of requirement 
+            rather than genuine enjoyment. From the moment I began this project to the day I deployed it, I was fully invested, 
+            and excited to make progress everyday. This project has made me level up as a developer, and I have already applied 
+            many of the lessons I've learned here to other work. I am eager to keep building on it and see how my skills evolve over time.
+          </p>
+          <div className="project-img-container">
+            <img src="../../assets/sr-deployment-day.png" className="project-img"/>
+            <p className="project-img-desc">Very special thank you to Geoff and Kaitlyn for making my deployment day so special!</p>
+          </div>
         </div>
         <footer className="guide-footer">
           <button className="right-guide-button" onClick={() => navigate('/projects/submitty')}>

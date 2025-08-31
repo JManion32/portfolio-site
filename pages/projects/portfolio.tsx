161f9761f772d3b8378470798d944e399b513029
Justin Manion <jmanion32@gmail.com>
2025-08-31 14:26:58 -0400

    Updated portfolio page
    Wrote it all out, and also slightly increased the h3 size, and letter spacing

--- pages/projects/portfolio.tsx
+++ pages/projects/portfolio.tsx
@@ -25,7 +25,7 @@ function Portfolio() {
             </button>
           </div>
           <div id="text-body">
-            <p>The site you are on right now, for showcasing my work. I hope you are enjoying it!</p>
+            <p>The site you are on right now, for showcasing my experience. I hope you are enjoying it!</p>
             <hr/>
             <TechStack
               sections={[
@@ -57,10 +57,57 @@ function Portfolio() {
                 }
               ]}
             />
+            <hr/>
+            <h2>Design Goals</h2>
             <p>
-              Pretty much the same stack as <a href="">Speed Roulette</a>, without the backend stuff.
+              There were several objectives I had in mind when designing this site:
+            </p>
+            <h3>Resume, but better</h3>
+            <p>
+              A resume is limited to only 1 page of content. Its strength lies in conciseness, 
+              but that can also be a limitation. With this site, I wanted to go beyond 
+              bullet points and offer a more detailed view of what I’ve been working on. 
+              In addition to showcasing my experience, the site also serves as a personal 
+              record of my progress. Most of all, it’s something I created for my father, 
+              a fellow software engineer, who always enjoys seeing what I’ve been building.
+            </p>
+            <h3>Responsive / Interactive Experience</h3>
+            <p>
+              Since this site is static with a focus on text content rather than defining 
+              features, I wanted to ensure it was thoughtfully designed. You'll find 
+              a smooth dark mode toggle, responsive scaling, fun animations, and 
+              interactive buttons, all crafted with care to make the UX both engaging and memorable.
+            </p>
+            <h3>Show Personality</h3>
+            <p>
+              This site is an opportunity to be personable rather than bland, or AI generated. I 
+              have been telling myself since the start that this is <code>justinmanion.com</code>, 
+              and I am not going to be afraid to let it reflect who I am.
             </p>
             <hr/>
+            <h2>Implementation</h2>
+            <p>
+              After weighing different options for the tech stack, I decided to use the same one as <a href="/projects/speedroulette">Speed Roulette</a>. 
+              I almost chose Vue, but decided React would be better for learning purposes. I also went back 
+              and forth for about using Tailwind or vanilla CSS, and eventually decided on the latter. 
+              This decision paid off as it was much more straightforward to handle the dark mode toggle and the responsive scaling.
+            </p>
+            <p>
+              While <a href="/projects/speedroulette">Speed Roulette</a> introduced me to React and large-scale CSS usage, this portfolio site 
+              solidified my understanding. I feel much more confident in how I structure and use components, and I’m 
+              confident this site will be easy to maintain going forward. For CSS, I thought I was already a pro, but this
+              project taught me a whole lot more. Before, I didn't realize I could create variables for more than just colors, 
+              but now I don't know how I lived without it. The variables were especially useful for the dark 
+              mode toggle and responsive scaling. I now feel like I have to go back through my other projects and 
+              add a <code>scaling.css</code> to them!
+            </p>
+            <hr/>
+            <h2>Reflection</h2>
+            <p>
+              Overall, building this site was fun and gave me a chance to solidify my knowledge of front-end 
+              technologies. Honestly, the hardest part was writing all of these pages! I hope you’ve enjoyed 
+              the experience so far!
+            </p>
           </div>
         </div>
         <NavButton

620c0ab080c843b81ac342b362dbe0d8f86e88ef
Justin Manion <jmanion32@gmail.com>
2025-09-04 15:43:15 -0400

    Update Stewarts.tsx

--- pages/experience/Stewarts.tsx
+++ pages/experience/Stewarts.tsx
@@ -11,7 +11,26 @@ function Stewarts() {
             <h1 className="text-page-header">IT Field Technician Intern</h1>
           </div>
           <div id="text-body">
+            <p>
+              During the summer of 2024, I had an internship as an Information Technology Field Technician for 
+              Stewart's Shops, a gas station chain in the Northeast. This role was hardware-focused, 
+              with a focus on maintenance and troubleshooting.
+            </p>
+            <hr/>
+            <h2>Overview</h2>
+            <p>
 
+            </p>
+            <hr/>
+            <h2>Responsibilities</h2>
+            <p>
+              
+            </p>
+            <hr/>
+            <h2>Reflection</h2>
+            <p>
+
+            </p>
           </div>
         </div>
         <NavButton

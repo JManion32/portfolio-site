39c0cffa0b40b8886a2f43cab1520b18c49317d2
Justin Manion <jmanion32@gmail.com>
2025-09-04 08:58:56 -0400

    Update HudsonValley.tsx

--- pages/experience/HudsonValley.tsx
+++ pages/experience/HudsonValley.tsx
@@ -9,10 +9,26 @@ function HudsonValley() {
           <Breadcrumbs />
           <div className="text-page-header-row">
             <h1 className="text-page-header">Hudson Valley Community College</h1>
-          </div>
-          <div id="text-body">
+        </div>
+        <div id="text-body">
+          <p>
+            When I enrolled in community college, I was unsure of what I wanted to pursue. Throughout 
+            the next two years, I would discover my interest in programming, and would lay a strong foundation 
+            for my transfer to university.
+          </p>
+          <hr/>
+          <h2>Freshman Year</h2>
+          <p>
 
-          </div>
+          </p>
+          <hr/>
+          <h2>Sophomore Year</h2>
+          <p>
+
+          </p>
+          <hr/>
+          <h2>Reflection</h2>
+        </div>
         </div>
         <NavButton
           left="Price Chopper" leftNav="/experience/pricechopper"

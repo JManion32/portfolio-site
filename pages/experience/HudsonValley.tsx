e810bcf0cb42a523544c6d7690291f2a3efc8bc8
Justin Manion <jmanion32@gmail.com>
2025-09-09 13:20:40 -0400

    Update HudsonValley.tsx

--- pages/experience/HudsonValley.tsx
+++ pages/experience/HudsonValley.tsx
@@ -21,7 +21,7 @@ function HudsonValley() {
           <hr/>
           <h2>Freshman Year</h2>
           <p>
-            The main interest I had that seemed like a valid field of study in college was building computers. Because of 
+            One of my favorite hobbies at the time was building computers. Because of 
             this and the fact that my dad is a software engineer, I decided to take an introductory computer science 
             course. I found enjoyment in the hardware side of things, but struggled with the software since it was brand 
             new to me. By the end of that semester, I switched my major from undecided to computer science and truly 
@@ -43,42 +43,32 @@ function HudsonValley() {
             I didn't focus up.
           </p>
           <hr/>
-          <h2>Sophomore Year</h2>
+          <h2>Sophomore Fall</h2>
           <p>
             With the introductory courses behind me, it was time to move on to the more challenging material. 
-            In my first semester, the main courses on my schedule were Calculus II and Programming and Data 
+            The main courses on my radar for this fall were Calculus II and Programming and Data 
             Logic II (HVCC’s version of data structures). That plan quickly changed when I realized I wasn’t 
             ready for Calculus II and switched to Calculus I, despite already having credit from high school. 
           </p>
           <h3>Programming and Data Logic 2</h3>
           <p>
-            Another online course. The language was Java, and there was a vast amount of foundational programming concepts 
-            covered here. With only a limited understanding of Python, this was going to be a challenge. I started with the only 
-            study habit I knew: scribing seven 60+ page slide decks into my notebook. I never looked at those notes again.
-          </p>
-          <p>
-            Seventy percent of Programming and Logic was our weekly projects, and they were not easy. This course built off of the 
-            fundamentals that Python Programming established (you know, the course I didn’t learn much from) and so the projects 
-            were more challenging. Nothing made any sense, and now that I was actually committed to learning, it was frustrating. 
-            Why hadn't I taken this seriously before? What was I thinking? These were good questions. I had been foolish, but 
+            Seventy percent of Programming and Logic was our weekly projects, and they were challenging. Everything built off 
+            of the course I had taken previously, yet nothing seemed to make sense. It was frustrating. 
+            Why hadn't I taken this seriously before? What was I thinking? I had been foolish, but 
             there was no time to dwell. The assignments were due on Mondays, and I struggled through them each weekend, staying 
-            up late and trying to understand what I was doing while making zero progress. It just wouldn’t click for me, but I 
-            knew I had to push through it. I wanted this to be my career, being lazy was no longer an option. Thankfully my 
-            Dad would spend hours working with me every Monday helping me through my assignments while making sure that I 
+            up late and trying to understand what I was doing while making zero progress. It just wouldn’t click for me, but this was the price 
+            I payed for not taking the introductory course seriously. If I wanted this to be my career, being lazy was no longer an option. Thankfully my 
+            Dad would spend hours working with me every Monday, helping me through my assignments while making sure I 
             understood them. I am beyond thankful for his efforts during this time. If it wasn’t for him, I probably would have given up.
           </p>
           <p>
             After spending the first half of the semester handing in assignments on the day they were due and attempting to learn 
-            what I should’ve already known, things were actually starting to click. New topics were becoming easier to grasp 
+            what I should’ve already known, things started to click. New topics were becoming easier to grasp 
             and I was no longer depending on my Dad to help me with the work. The course covered loads of core concepts each 
             week including data structures, generics, recursion, exception handling, inheritance, abstraction, encapsulation, 
             polymorphism, and more. These topics were cool and all, but my favorite by far was JavaFX. JavaFX is a toolkit 
-            for Java that allows you to create a Graphical User Interface, also known as a GUI. We learned the basics of creating 
-            user interfaces such as grids, buttons, and textfields, as well as how to utilize CSS to make it look pretty. When 
-            I streamed on Twitch, I would spend hours fine tuning my overlays to exactly how I wanted them. I found it so cool 
-            to be able to do this with a computer application where I could not just move things around and resize them, but 
-            also create the functionality. This was my first taste of front end development, but definitely not the last. By the 
-            end of the course, I had finally developed a coding skill set. It had only taken 3 semesters, but I could now solve 
+            for Java that allows you to create a Graphical User Interface. This was my first taste of front end development, but definitely not the last. By the 
+            end of the course, I had finally developed the foundation of my coding skill set. It had taken 3 semesters, but I could finally solve 
             problems on my own with code, and that felt pretty awesome. 
           </p>
           <h3>Calculus I</h3>
@@ -95,11 +85,14 @@ function HudsonValley() {
             the low 70s heading into the final. I needed at least a C to receive credit for this course, so the pressure was on. 
           </p>
           <p>
-            I went into the final exam room confident and composed. I had prepared a massive double sided crib sheet with just about every 
-            topic I could possibly need on it. Even if I didn’t know the material, it was on the papaer. I started off strong, still having 
-            a solid grasp on the exam 1 concepts. This confidence dwindled as I dove further into the packet. I skipped a page, only to skip the 
-            next one, and so on until I realized that I was only able to do half of the packet. By the end, I had lost all confidence. 
-            I had come up short, and I knew it.
+            I walked into the final exam confident and composed, armed with a massive double-sided crib sheet packed with nearly every topic 
+            I could think of. Even if I didn’t know the material, I had it written down. At first, things went smoothly; I still had a solid 
+            grasp of the exam one concepts. But as I moved deeper into the packet, my confidence began to fade. I skipped one page, then another, 
+            until I realized I could only answer about half the exam. By the end, I had lost all confidence. I had come up short, and I knew it.
+          </p>
+          <p>
+            This was my exact issue with school. I spend an entire day making a crib sheet, rather than actually studying. It was the easy way out. 
+            Looking back, I'm glad that the consequences caught up with me here.
           </p>
           <p>
             It was a true reality check. In the following week, 
@@ -112,17 +105,30 @@ function HudsonValley() {
             It was official. I had failed. The 4.0 was no more. I went to DegreeWorks to see the damage, only to make a shocking revelation. 
             Since I already had credit for the course, the transcript just took the better score. All of my struggles were in vain, 
             or were they? I had still learned 68% of the material, and received the reality check that I so desparately needed. 
-            It was now up to me to conquer the looming 20 credit spring semester that included Calculus II.
+            With a clean slate, it was now up to me to conquer the looming 20 credit spring semester that included Calculus II.
           </p>
-          <h3>The Final Semester</h3>
+          <hr/>
+          <h2>The Final Semester</h2>
           <p>
             It was time to finish strong. I was taking 20 credits, with key courses including Calculus II, Assembly Language, 
-            and Discrete Mathematics. I wouldn't describe myself as confident going in, I was just envigorated by the chance to prove to myself 
+            and Discrete Mathematics. I wouldn't describe myself as confident going in, I was just invigorated by the chance to prove 
             that I could rise to the challenge.
           </p>
           <p>
-            With new study habits in place and a continuous drive to learn, this semester was a victory lap. I received an A in four of 
-            my five courses. The only course I missed was Calculus II, where I received a B, which was an amazing result.
+          </p>
+          <h3>Calculus II</h3>
+          <p>
+            It is said that this course is the hardest of the calcs, and throughout this semester, I learned why. On the first day, 
+            my professor said that students usually get one letter grade lower in Calc 2 than calc 1 which, in my case, was an F. 
+            That was a little scary to hear, but there was no more safetly nets, I just had to do it. When the dust settled, 
+            I had just barely scraped a B, a result that I was beyond content with.
+          </p>
+          <h3>The Rest</h3>
+          <p>
+            This was also my first semester where most of my classes were held in person, and I found myself enjoying the experience. 
+            Discrete Mathematics and Assembly Language stood out in particular, both of which gave me valuable preparation for RPI. 
+            I also made friends who would eventually transfer alongside me, making the transition even smoother. 
+            Altogether, it was a great semester, and I wrapped up community college with a 3.94 GPA.
           </p>
           <hr/>
           <h2>Reflection</h2>

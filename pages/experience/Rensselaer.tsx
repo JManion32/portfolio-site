0d52b77e5bb5e98384649d1cec3fbe50d52e8cc5
JManion32 <jmanion32@gmail.com>
2025-12-28 12:14:06 -0500

    Created RPI collage

--- pages/experience/Rensselaer.tsx
+++ pages/experience/Rensselaer.tsx
@@ -5,8 +5,37 @@ import Breadcrumbs from '../../components/Breadcrumb';
 import NavButton from '../../components/NavButton';
 import useNavigateTop from "../../hooks/useNavigateTop";
 
-import rpiFall from '../../assets/rpi-fall.png';
 import summerSage from '../../assets/summer-sage.jpg';
+
+import rpi1 from '../../assets/RPI_1.jpg';
+import rpi2 from '../../assets/RPI_2.jpg';
+import rpi3 from '../../assets/RPI_3.jpg';
+import rpi4 from '../../assets/RPI_4.jpg';
+import rpi5 from '../../assets/RPI_5.jpg';
+import rpi6 from '../../assets/RPI_6.jpg';
+import rpi7 from '../../assets/RPI_7.jpg';
+import rpi8 from '../../assets/RPI_8.jpg';
+import rpi9 from '../../assets/RPI_9.jpg';
+import rpi10 from '../../assets/RPI_10.jpg';
+import rpi11 from '../../assets/RPI_11.jpg';
+import rpi12 from '../../assets/RPI_12.jpg';
+import rpi13 from '../../assets/RPI_13.jpg';
+import rpi14 from '../../assets/RPI_14.jpg';
+import rpi15 from '../../assets/RPI_15.jpg';
+import rpi16 from '../../assets/RPI_16.jpg';
+import rpi17 from '../../assets/RPI_17.jpg';
+import rpi18 from '../../assets/RPI_18.jpg';
+import rpi19 from '../../assets/RPI_19.jpg';
+import rpi20 from '../../assets/RPI_20.jpg';
+import rpi21 from '../../assets/RPI_21.jpg';
+import rpi22 from '../../assets/RPI_22.jpg';
+import rpi23 from '../../assets/RPI_23.jpg';
+import rpi24 from '../../assets/RPI_24.jpg';
+import rpi25 from '../../assets/RPI_25.jpg';
+import rpi26 from '../../assets/RPI_26.jpg';
+import rpi27 from '../../assets/RPI_27.jpg';
+import rpi28 from '../../assets/RPI_28.jpg';
+import rpi29 from '../../assets/RPI_29.jpg';
 
 function Rensselaer() {
   const navigateTop = useNavigateTop();
@@ -34,7 +63,7 @@ function Rensselaer() {
               <p>
                 Transferring from community college to RPI was a massive adjustment. My first semester was 
                 exciting, but stressful. I was finding my footing on campus, rushing <a onClick={() => navigateTop("/experience/lambdachi")}>Lambda Chi Alpha</a>, 
-                and commuting from home all while trying to keep up with the rigor of my courses. I took four courses totaling 16 credits. They were: Physics I, Multivariable Calculus, Writing 
+                and commuting from home all while trying to keep up with the rigor of my courses. I took 4 courses totaling 16 credits. They were: Physics I, Multivariable Calculus, Writing 
                 for Technical Communication, and the most impactful of all:
               </p>
               <h3>Data Structures</h3>
@@ -91,11 +120,36 @@ function Rensselaer() {
               <p>
                 RPI is awesome.
               </p>
-              <div id="content-img-container">
-                <img src={rpiFall} className="project-img"/>
-                <p>
-                  Especially in the fall.
-                </p>
+              <div id="rpi-collage-container">
+                <img src={rpi1} className="rpi-collage-img"/>
+                <img src={rpi2} className="rpi-collage-img"/>
+                <img src={rpi3} className="rpi-collage-img"/>
+                <img src={rpi4} className="rpi-collage-img"/>
+                <img src={rpi5} className="rpi-collage-img"/>
+                <img src={rpi6} className="rpi-collage-img"/>
+                <img src={rpi7} className="rpi-collage-img"/>
+                <img src={rpi8} className="rpi-collage-img"/>
+                <img src={rpi9} className="rpi-collage-img"/>
+                <img src={rpi10} className="rpi-collage-img"/>
+                <img src={rpi11} className="rpi-collage-img"/>
+                <img src={rpi12} className="rpi-collage-img"/>
+                <img src={rpi13} className="rpi-collage-img"/>
+                <img src={rpi14} className="rpi-collage-img"/>
+                <img src={rpi15} className="rpi-collage-img"/>
+                <img src={rpi16} className="rpi-collage-img"/>
+                <img src={rpi17} className="rpi-collage-img"/>
+                <img src={rpi18} className="rpi-collage-img"/>
+                <img src={rpi19} className="rpi-collage-img"/>
+                <img src={rpi20} className="rpi-collage-img"/>
+                <img src={rpi21} className="rpi-collage-img"/>
+                <img src={rpi22} className="rpi-collage-img"/>
+                <img src={rpi23} className="rpi-collage-img"/>
+                <img src={rpi24} className="rpi-collage-img"/>
+                <img src={rpi25} className="rpi-collage-img"/>
+                <img src={rpi26} className="rpi-collage-img"/>
+                <img src={rpi27} className="rpi-collage-img"/>
+                <img src={rpi28} className="rpi-collage-img"/>
+                <img src={rpi29} className="rpi-collage-img"/>
               </div>
             </div>
           </div>

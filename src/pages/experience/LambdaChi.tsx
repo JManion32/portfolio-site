0f4ac583e504b73a7a2ac195d5d082d1a3b94efd
JManion32 <jmanion32@gmail.com>
2026-06-17 17:39:25 -0400

    Updated LXA

--- src/pages/experience/LambdaChi.tsx
+++ src/pages/experience/LambdaChi.tsx
@@ -44,7 +44,7 @@ export default function LambdaChi() {
                         <h2>Secretary</h2>
                         <p>As Secretary, I handled administrative operations to keep our chapter running smoothly.</p>
                         <p>
-                            One of the more front-facing responsibilities I had was managing weekly house meetings.
+                            One of my core responsibilities was managing weekly house meetings.
                             Before each meeting, I emailed a reminder to the brotherhood, and prepared the slideshow,
                             officer agenda, and meeting minutes. I took attendance at the start, recorded the minutes
                             during the meeting, finalized them afterward, and sent them out promptly. Though this
@@ -58,24 +58,20 @@ export default function LambdaChi() {
                         <hr />
                         <h2>Director of Recruitment Logistics</h2>
                         <p>
-                            Organization is the backbone of successful recruitment. Responsibilities included. preparing
-                            bid packets for new members, and printing flyers. In addition, I became a member of the
+                            Organization is the backbone of successful recruitment. Responsibilities included preparing
+                            bid packets for new members, and printing flyers. I also served as a member of the
                             recruitment committee, where we discussed ideas and strategies.
                         </p>
                         <hr />
                         <h2>Webmaster</h2>
                         <p>
-                            In recent years, the Webmaster role has been neglected, leaving the site outdated and
-                            lacking relevant information. I have worked to bring everything up to date, and establish a
-                            system to prevent this from happening again. Before I transition out of the role, I will
-                            create a document that will give the inheritor everything necessary to succeed.
+                            When I picked up the Webmaster role, it had been neglected for several years. Our site was out of 
+                            date, we lacked a steady flow of content, and the position was just generally overlooked.
                         </p>
                         <p>
-                            My ongoing priority is repopulating the site with new event posts, chapter news, and
-                            brotherhood achievements. Beyond this, I aim to add a gallery page showcasing the thousands
-                            of digitized photos from our archives as well as a dedicated history page celebrating our
-                            chapter’s legacy. My goal is to preserve our story and make it easily accessible to all
-                            brothers—past, present, and future.
+                            As the Webmaster, I revitalized the site, wrote weekly posts about chapter events, 
+                            overhauled various pages, and created an extensive transition document to make it easier 
+                            for my predecessors to maintain the site.
                         </p>
                         <hr />
                         <h2>Philanthropy</h2>
@@ -103,8 +99,8 @@ export default function LambdaChi() {
                         <h2>Reflection</h2>
                         <p>
                             Being a part of something greater than myself has turned the grind of college into a unique
-                            and enjoyable experience. I am grateful to be a part of this amazing brotherhood, and
-                            excited for what lies ahead.
+                            and enjoyable experience. I am grateful to have experienced this amazing brotherhood, and
+                            excited for what lies ahead for us beyond RPI.
                         </p>
                     </>
                 }
